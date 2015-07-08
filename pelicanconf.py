8c7d842b8b79b70e4ca2f678ddaa139e4eca78a8
Magnun Leno <magnun.leno@gmail.com>
2015-07-07 23:38:44 -0300

    Renomeia Artigos para Blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,19 +12,19 @@ THEME = "./.themes/materialize"
 
 DEFAULT_LANG = u'pt'
 
-ARTICLE_URL = 'artigos/{slug}'
-ARTICLE_SAVE_AS = 'artigos/{slug}.html'
+ARTICLE_URL = 'blog/{slug}'
+ARTICLE_SAVE_AS = 'blog/{slug}.html'
 
 PAGE_URL = '{slug}'
 PAGE_SAVE_AS = '{slug}.html'
 
-CATEGORY_URL = 'artigos/category/{slug}'
-CATEGORY_SAVE_AS = 'artigos/category/{slug}.html'
+CATEGORY_URL = 'blog/category/{slug}'
+CATEGORY_SAVE_AS = 'blog/category/{slug}.html'
 
-TAG_URL = 'artigos/tag/{slug}'
-TAG_SAVE_AS = 'artigos/tag/{slug}.html'
+TAG_URL = 'blog/tag/{slug}'
+TAG_SAVE_AS = 'blog/tag/{slug}.html'
 
-INDEX_SAVE_AS = "artigos/index.html"
+INDEX_SAVE_AS = "blog/index.html"
 
 #PAGINATION_PATTERNS = (
 #    (1, '{base_name}/', '{base_name}/index.html'),
@@ -43,8 +43,8 @@ DEFAULT_PAGINATION = False
 # Navbar Links
 NAVBAR_LINKS = (
     {
-        "title" : "Artigos",
-        "href": "artigos",
+        "title" : "Blog",
+        "href": "blog",
     },
     {
         "title" : "Membros",
